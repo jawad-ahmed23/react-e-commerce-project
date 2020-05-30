ca2128e48e3bdf4054b1e147c1dfb8b994137c4a
Jawad Ahmed <jawad-ahmed23@users.noreply.github.com>
2020-05-30 15:46:21 +0500

    added header component in the app

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Switch, Route } from "react-router-dom";
 //Custom Components
 import HomePage from "./pages/Homepage/Homepage.pages";
 import ShopPage from "./pages/shop/shop.pages";
+import Header from "./components/header/header.component";
 
 //CSS
 import "./App.css";
@@ -13,6 +14,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/shop" component={ShopPage} />
--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -4,7 +4,7 @@ import "./collection-preview.styles.scss";
 
 const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
-    <h1>{title}</h1>
+    <h1 className="title">{title}</h1>
     <div className="preview">
       {items
         .filter((item, i) => i < 4)
